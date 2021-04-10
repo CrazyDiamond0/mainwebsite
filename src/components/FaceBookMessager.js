1187adfab6acb1cbc45683ab9c83d45789fdc676
CrazyDiamond0 <popovici.alex96@gmail.com>
2021-04-10 12:44:12 +0300

    pageId changed

--- src/components/FaceBookMessager.js
+++ src/components/FaceBookMessager.js
@@ -4,7 +4,7 @@ import MessengerCustomerChat from 'react-messenger-customer-chat';
 export default function FacebookMessager(){
     return(
     <MessengerCustomerChat
-        pageId="100006254024933"
+        pageId="100656702153759"
         appId="1010657329738015"
       />)
 
