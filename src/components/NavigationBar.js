faa59e8558b47681d6f7c3e9c6fb48e5859a7bb2
CrazyDiamond0 <popovici.alex96@gmail.com>
2021-04-10 11:37:34 +0300

    added links to nagiationbar and footer

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -10,7 +10,7 @@ function NavigationBar(){
     <nav className="navbar navbar-expand-lg navbar-light bg-dark">
         <div className="container"> 
 
-    <a className="navbar-brand" href="#"><img className='logo' src={logo} alt='...logo'/></a>
+    <a className="navbar-brand" href="https://www.instagram.com/alex.al.pop/"><img className='logo' src={logo} alt='...logo'/></a>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -18,19 +18,19 @@ function NavigationBar(){
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav ml-auto">
         <li className="nav-item active">
-          <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+          <a className="nav-link" href="https://www.instagram.com/alex.al.pop/">Home <span className="sr-only">(current)</span></a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">About</a>
+          <a className="nav-link" href="https://www.instagram.com/alex.al.pop/">About</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Projects</a>
+          <a className="nav-link" href="https://www.instagram.com/alex.al.pop/">Projects</a>
         </li>
         <li className="nav-item">
           <a className="nav-link" href="/resume.pdf" download>Resume</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Contact</a>
+          <a className="nav-link" href="https://www.instagram.com/alex.al.pop/">Contact</a>
         </li>
         
       </ul>
