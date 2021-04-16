ce708ea03f2dbc7418c4233edbc5c6c306685a0a
CrazyDiamond0 <popovici.alex96@gmail.com>
2021-04-16 18:37:54 +0300

    new facebook settings

--- src/components/FaceBookMessager.js
+++ src/components/FaceBookMessager.js
@@ -3,6 +3,6 @@ import MessengerCustomerChat from "react-messenger-customer-chat";
 
 export default function FacebookMessager() {
   return (
-    <MessengerCustomerChat pageId="100656702153759" appId="1010657329738015" />
+    <MessengerCustomerChat pageId="104959348393978" appId="1010657329738015" />
   );
 }
