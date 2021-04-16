e5a0989e1ba6cc6c0c56b3d37bdf2b74ca12f785
CrazyDiamond0 <popovici.alex96@gmail.com>
2021-04-16 16:35:25 +0300

    added projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,7 @@ import { Card, Container, Carousel } from "react-bootstrap";
 import "../index.css";
 import mainpage from "../resources/mainpage.png";
 import chatapp from "../resources/chatapp.png";
+import arithmeticapp from "../resources/arithmeticapp.png";
 import { motion } from "framer-motion";
 
 export default function Projects() {
@@ -28,11 +29,31 @@ export default function Projects() {
               <h3>Main Website</h3>
             </Carousel.Caption>
           </Carousel.Item>
+
+          {/*https://drive.google.com/file/d/1MUsXjd5Df-VLBEv_siku-cUqLz3MPcNH/view?usp=sharing*/}
+
           <Carousel.Item interval={5000}>
             <motion.div whileHover={{ opacity: 0.5, scale: 1.2 }}>
               <div>
-                <a href="https://chattingapp-e6186.web.app/">
+                <a href="https://drive.google.com/file/d/1MUsXjd5Df-VLBEv_siku-cUqLz3MPcNH/view?usp=sharing">
                   <Card.Img variant="top" src={chatapp} />
+                </a>
+              </div>
+            </motion.div>
+            <Carousel.Caption>
+              <h3>Arithmetic Operations App</h3>
+              <p>
+                App that uses arithmetic operations in order to identify the
+                movable objects in series of frames
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+
+          <Carousel.Item interval={5000}>
+            <motion.div whileHover={{ opacity: 0.5, scale: 1.2 }}>
+              <div>
+                <a href="https://drive.google.com/file/d/1MUsXjd5Df-VLBEv_siku-cUqLz3MPcNH/view?usp=sharing">
+                  <Card.Img variant="top" src={arithmeticapp} />
                 </a>
               </div>
             </motion.div>
