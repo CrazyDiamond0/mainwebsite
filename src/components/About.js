5b48ce91c86a5ca329c57f6a881062e287c0e24d
CrazyDiamond0 <popovici.alex96@gmail.com>
2021-04-16 18:22:54 +0300

    fixing warnings

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+//import { Container, Row, Col } from "react-bootstrap";
 import logo from "../resources/mainpicture.jpg";
 
 export default function About() {
